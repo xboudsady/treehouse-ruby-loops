887699d50b2fea30e969f16e8c40c0f7a904266e
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-06-29 15:59:38 -0700

    added for_loop.rb

---
+++ for_loop.rb
@@ -0,0 +1,9 @@
+# The for loop in ruby
+
+for item in 1..3 do                        # Ruby for loop, using a range, two different obect, make something like an array which we can iterate over
+    puts "The current item is #{item}"      
+end
+
+for item in ["Programming", "is", "fun"]
+    puts "The current item is #{item}"
+end
