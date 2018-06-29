4aebf77619e48d0c58c9061fd2766c31a6fb3800
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-06-29 15:48:27 -0700

    Added times.rb & times_with_arguments.rb

---
+++ times.rb
@@ -0,0 +1,5 @@
+# Using the .times method in a do loop
+
+5.times do                      # .times method
+    puts "Hello!"
+end
---
+++ times_with_arguments.rb
@@ -0,0 +1,5 @@
+# Using the .times method along with an arguments
+
+5.times do |item|               # .tmes method with do loop and arguments
+    puts "Hello! #{item}"       # item starts with zero, just like array index
+end
